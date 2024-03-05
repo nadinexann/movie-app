01465e73bcf76b45b270060952bb5d6f757c007b
Milos Stojsavljevic <Milos.Stojsavljevic@kontron.com>
2024-03-05 10:12:33 +0100

    Add new project
    Signed-off-by: Milos Stojsavljevic <Milos.Stojsavljevic@kontron.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from './services/movie.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'movie-app';
+export class AppComponent implements OnInit {
+  constructor(private movieService: MovieService) {}
+
+  ngOnInit(): void {
+    this.movieService
+      .getAllMovies()
+      .subscribe((result) => console.log(result, 'RESULT'));
+  }
 }
