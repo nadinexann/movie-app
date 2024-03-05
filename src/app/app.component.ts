6dedf92f1ed74abe4702ac6f04965fe4762df3f3
Villaluz Nadine-Ann <Nadine-Ann.Villaluz@kontron.com>
2024-03-05 08:04:13 +0100

    add the project
    Signed-off-by: Villaluz Nadine-Ann <Nadine-Ann.Villaluz@kontron.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,12 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'my-app nadine';
+
+  public getName() {
+    return this.title;
+  }
 }
